459f5096000b436fe31f1a8bfc175fa738f9bc10
Amrit Sharma <amrit.sharma4@wipro.com>
2022-09-06 13:56:44 +0530

    added n-carbon-components-react

--- react-app/index.tsx
+++ react-app/index.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.scss";
 
 import { Hello, HelloProps } from "./components/Hello";
 
 (window as any).mount = (el: any, injected: HelloProps) => {
+  console.log(injected);
 
-    console.log(injected)
-
-    ReactDOM.render(
-        <Hello {...injected} />,
-        el
-    );
-}
-
+  ReactDOM.render(<Hello {...injected} />, el);
+};
